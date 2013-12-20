c9aeb9b46b6d290d7446419629481dbfa2dd128d
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2013-12-20 19:26:52 +0100

    Pass process name to trace on command line

--- examples/function-tracing/funtracer.py
+++ examples/function-tracing/funtracer.py
@@ -4,8 +4,12 @@ import frida
 import tracer
 
 if __name__ == '__main__':
-    tp = tracer.TracerProfileBuilder().include("read").build()
+    if len(sys.argv) != 2:
+        print "Usage: %s <process name>" % sys.argv[0]
+        sys.exit(1)
+    tp = tracer.TracerProfileBuilder().include_modules("*").exclude_modules("libSystem*").build()
     t = tracer.Tracer(tp)
-    p = frida.attach("cat")
+    p = frida.attach(sys.argv[1])
     t.start_trace(p, tracer.STDOUT_SINK)
     sys.stdin.read()
+    sys.exit(0)
