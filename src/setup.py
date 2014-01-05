e60ef52fee1a717514d2b70fbf16f82305139575
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2014-01-05 14:32:12 +0100

    Improve PyPI metadata

--- src/setup.py
+++ src/setup.py
@@ -24,24 +24,53 @@ class FridaPrebuiltExt(build_ext):
 setup(
     name='frida',
     version=version,
-    packages=['frida'],
-    license="GNU GPLv3",
-    description="Frida is an open-source toolkit for interactive and scriptable reverse-engineering",
+    description="Inject JavaScript code to explore native apps on Windows, Mac, Linux and iOS",
     long_description=long_description,
-    url="http://frida.github.io",
     author="Frida Developers",
     author_email="ole.andre.ravnas@tillitech.com",
+    url="http://frida.github.io",
+    install_requires=[
+        "colorama >= 0.2.7"
+    ],
+    license="GNU GPLv3+",
+    zip_safe=True,
+    keywords="frida debugger inject javascript windows mac ios iphone ipad",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Environment :: MacOS X",
+        "Environment :: Win32 (MS Windows)",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Microsoft :: Windows :: Windows XP",
+        "Operating System :: Microsoft :: Windows :: Windows 7",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: JavaScript",
+        "Topic :: Software Development :: Debuggers",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    packages=['frida'],
     entry_points={
         'console_scripts': [
-            'frida-discover = frida.discoverer:main',
-            'frida-trace = frida.tracer:main'
+            "frida-discover = frida.discoverer:main",
+            "frida-trace = frida.tracer:main"
         ]
     },
     ext_modules=[Extension('_frida', [])],
     cmdclass={
         'build_ext': FridaPrebuiltExt
-    },
-    install_requires=[
-        "colorama >= 0.2.7"
-    ]
+    }
 )
