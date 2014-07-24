eafb5a4b4362d4fb2a8c2267c50a27a86d90f56a
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2014-07-24 22:05:36 +0200

    Display some troubleshooting tips when the _frida import fails

--- src/frida/__init__.py
+++ src/frida/__init__.py
@@ -19,7 +19,28 @@ def get_device_manager():
     global _device_manager
     if _device_manager is None:
         from . import core
-        import _frida
+        try:
+            import _frida
+        except Exception as ex:
+            import colorama
+            from colorama import Back, Fore, Style
+            import sys
+            colorama.init(autoreset=True)
+            print("")
+            print("***")
+            if str(ex).startswith("No module named "):
+                print(Back.RED + Fore.WHITE + Style.BRIGHT + "Frida native extension not found")
+                print(Fore.WHITE + Style.BRIGHT + "Please check your PYTHONPATH.")
+            else:
+                print(Back.RED + Fore.WHITE + Style.BRIGHT + "Failed to load the Frida native extension: %s" % ex)
+                if sys.version_info[0] == 2:
+                    current_python_version = "%d.%d" % sys.version_info[:2]
+                else:
+                    current_python_version = "%d.x" % sys.version_info[0]
+                print(Fore.WHITE + Style.BRIGHT + "Please ensure that the extension was compiled for Python " + current_python_version + ".")
+            print("***")
+            print("")
+            raise ex
         _device_manager = core.DeviceManager(_frida.DeviceManager())
         _device_manager.enumerate_devices() # warm up
     return _device_manager
