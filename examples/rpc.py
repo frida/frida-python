364c5803aac3ec69f6e50f0b37f6b48d9c2d6006
Ole André Vadla Ravnås <oleavr@gmail.com>
2016-06-02 14:44:34 +0200

    Add a tiny RPC example

---
+++ examples/rpc.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import frida
+
+session = frida.attach("Twitter")
+script = session.create_script("""\
+'use strict';
+
+rpc.exports = {
+  hello: function () {
+    return 'Hello';
+  },
+  failPlease: function () {
+    oops;
+  }
+};
+""")
+script.load()
+api = script.exports
+print("api.hello() =>", api.hello())
+api.fail_please()
