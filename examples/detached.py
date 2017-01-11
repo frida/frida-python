854a24a843eff6be2b4988a8c51b51d53b00cd96
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-01-11 03:50:46 +0100

    Add an example covering the `detached` signal

---
+++ examples/detached.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import frida
+import sys
+
+def on_detached():
+    print("on_detached")
+
+def on_detached_with_reason(reason):
+    print("on_detached_with_reason:", reason)
+
+def on_detached_with_varargs(*args):
+    print("on_detached_with_varargs:", args)
+
+session = frida.attach("Twitter")
+print("attached")
+session.on('detached', on_detached)
+session.on('detached', on_detached_with_reason)
+session.on('detached', on_detached_with_varargs)
+sys.stdin.read()
