759fa47f5e3da5dfb1c7f0dbe9af2837c25955d0
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-10-10 15:37:57 +0200

    Switch generated JavaScript to two space indentation
    As this is more common for JS code.

--- src/frida/tracer.py
+++ src/frida/tracer.py
@@ -614,8 +614,8 @@ class Repository(object):
             if len(args) == 0:
                 log_str = "\"%(name)s()\"" % { "name": function.name }
             else:
-                indent_outer = "        "
-                indent_inner = "            "
+                indent_outer = "    "
+                indent_inner = "      "
                 log_str = "\"%(name)s(\" +\n%(indent_inner)s%(args)s\n%(indent_outer)s\")\"" % {
                     "name": function.name,
                     "args": ("\n" + indent_inner).join(args),
@@ -632,35 +632,35 @@ class Repository(object):
  */
 
 {
-    /**
-     * Called synchronously when about to call %(display_name)s.
-     *
-     * @this {object} - Object allowing you to store state for use in onLeave.
-     * @param {function} log - Call this function with a string to be presented to the user.
-     * @param {array} args - Function arguments represented as an array of NativePointer objects.
-     * For example use Memory.readUtf8String(args[0]) if the first argument is a pointer to a C string encoded as UTF-8.
-     * It is also possible to modify arguments by assigning a NativePointer object to an element of this array.
-     * @param {object} state - Object allowing you to keep state across function calls.
-     * Only one JavaScript function will execute at a time, so do not worry about race-conditions.
-     * However, do not use this to store function arguments across onEnter/onLeave, but instead
-     * use "this" which is an object for keeping state local to an invocation.
-     */
-    onEnter: function (log, args, state) {
-        log(%(log_str)s);
-    },
+  /**
+   * Called synchronously when about to call %(display_name)s.
+   *
+   * @this {object} - Object allowing you to store state for use in onLeave.
+   * @param {function} log - Call this function with a string to be presented to the user.
+   * @param {array} args - Function arguments represented as an array of NativePointer objects.
+   * For example use Memory.readUtf8String(args[0]) if the first argument is a pointer to a C string encoded as UTF-8.
+   * It is also possible to modify arguments by assigning a NativePointer object to an element of this array.
+   * @param {object} state - Object allowing you to keep state across function calls.
+   * Only one JavaScript function will execute at a time, so do not worry about race-conditions.
+   * However, do not use this to store function arguments across onEnter/onLeave, but instead
+   * use "this" which is an object for keeping state local to an invocation.
+   */
+  onEnter: function (log, args, state) {
+    log(%(log_str)s);
+  },
 
-    /**
-     * Called synchronously when about to return from %(display_name)s.
-     *
-     * See onEnter for details.
-     *
-     * @this {object} - Object allowing you to access state stored in onEnter.
-     * @param {function} log - Call this function with a string to be presented to the user.
-     * @param {NativePointer} retval - Return value represented as a NativePointer object.
-     * @param {object} state - Object allowing you to keep state across function calls.
-     */
-    onLeave: function (log, retval, state) {
-    }
+  /**
+   * Called synchronously when about to return from %(display_name)s.
+   *
+   * See onEnter for details.
+   *
+   * @this {object} - Object allowing you to access state stored in onEnter.
+   * @param {function} log - Call this function with a string to be presented to the user.
+   * @param {NativePointer} retval - Return value represented as a NativePointer object.
+   * @param {object} state - Object allowing you to keep state across function calls.
+   */
+  onLeave: function (log, retval, state) {
+  }
 }
 """ % {"display_name": display_name, "log_str": log_str}
 
