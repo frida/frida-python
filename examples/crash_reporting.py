f238498cdc6a605a2709f58098a054449e2ed8a1
Ole André Vadla Ravnås <oleavr@gmail.com>
2019-02-04 14:26:58 +0100

    Add crash reporting example

---
+++ examples/crash_reporting.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import sys
+
+import frida
+
+
+def on_process_crashed(crash):
+    print("on_process_crashed")
+    print("\tcrash:", crash)
+
+def on_detached(reason, crash):
+    print("on_detached()")
+    print("\treason:", reason)
+    print("\tcrash:", crash)
+
+device = frida.get_usb_device()
+device.on('process-crashed', on_process_crashed)
+session = device.attach("Hello")
+session.on('detached', on_detached)
+print("[*] Ready")
+sys.stdin.read()
