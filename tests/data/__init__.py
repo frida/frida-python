7d57f6807f04b69c6a19dfa66799b994eacfc423
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-04-20 23:09:21 +0000

    Use our own test binaries on Linux also
    So we can pick the appropriate one based on whether we're running on
    a 32- or 64-bit Python interpreter. There's no need for our 32-bit
    builds to support injection into 64-bit targets.

--- tests/data/__init__.py
+++ tests/data/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import platform
 import os
+import platform
+import sys
+
 
 system = platform.system()
 if system == 'Windows':
@@ -9,6 +11,8 @@ if system == 'Windows':
 elif system == 'Darwin':
     target_program = os.path.join(os.path.dirname(__file__), "unixvictim-macos")
 else:
-    target_program = "/bin/cat"
+    arch = "x86_64" if sys.maxsize > 2**32 else "x86"
+    target_program = os.path.join(os.path.dirname(__file__), "unixvictim-linux-" + arch)
+
 
 __all__ = ['target_program']
