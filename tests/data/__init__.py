03288953b1677ff6e1f35a5d5717ea5b682cd997
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-11-02 17:00:21 +0100

    Add unixvictim binaries for Linux/arm{hf,64}

--- tests/data/__init__.py
+++ tests/data/__init__.py
@@ -11,7 +11,13 @@ if system == 'Windows':
 elif system == 'Darwin':
     target_program = os.path.join(os.path.dirname(__file__), "unixvictim-macos")
 else:
-    arch = "x86_64" if sys.maxsize > 2**32 else "x86"
+    machine = platform.machine()
+    if machine == 'aarch64':
+        arch = 'arm64'
+    elif machine.startswith('arm'):
+        arch = 'armhf'
+    else:
+        arch = 'x86_64' if sys.maxsize > 2**32 else 'x86'
     target_program = os.path.join(os.path.dirname(__file__), "unixvictim-linux-" + arch)
 
 
