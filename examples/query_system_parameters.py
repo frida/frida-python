d4a40b3d74d6c68d772f0ef09c3ab52fabecd634
Ole André Vadla Ravnås <oleavr@gmail.com>
2021-07-05 17:51:54 +0200

    Improve the query_system_parameters() example

--- examples/query_system_parameters.py
+++ examples/query_system_parameters.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 import frida
+from pprint import pformat
+from pygments import highlight
+from pygments.formatters import Terminal256Formatter
+from pygments.lexers import PythonLexer
 
 
-print("Local parameters:", frida.query_system_parameters())
-print("USB device parameters:", frida.get_usb_device().query_system_parameters())
+print("Local parameters:", highlight(pformat(frida.query_system_parameters()), PythonLexer(), Terminal256Formatter()))
+print("USB device parameters:", highlight(pformat(frida.get_usb_device().query_system_parameters()), PythonLexer(), Terminal256Formatter()))
