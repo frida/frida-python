86358c948cf5fb1594c1560ea11e363e4a14d794
GalaxySnail <ylc991@163.com>
2022-01-24 20:48:29 +0800

    setup: Use PEP 503 instead of PyPI xmlrpc (#188)
    To support mirrors.
    
    Fixes #141.

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
+import sys
+import os
+import platform
+import re
+import zipfile
+import shutil
+import struct
 import codecs
+import subprocess
+import hashlib
+from collections import namedtuple
+from functools import partial
 try:
     from io import BytesIO
 except:
@@ -9,26 +20,29 @@ except:
         from cStringIO import StringIO as BytesIO
     except:
         from StringIO import StringIO as BytesIO
-import os
-import platform
-import re
-from setuptools import setup
-from setuptools.command.build_ext import build_ext
-from setuptools.extension import Extension
-import shutil
-import struct
-import subprocess
-import sys
 try:
     from urllib.request import urlopen, Request
 except:
     from urllib2 import urlopen, Request
 try:
-    import xmlrpclib
-except ImportError:
-    import xmlrpc.client as xmlrpclib
-import zipfile
+    from urllib.parse import urljoin, urlparse
+except:
+    from urlparse import urljoin, urlparse
+try:
+    from html.parser import HTMLParser
+except:
+    from HTMLParser import HTMLParser
 
+from setuptools import setup
+from setuptools.command.build_ext import build_ext
+from setuptools.extension import Extension
+
+
+DEFAULT_INDEX_URL = "https://pypi.org/simple/"
+
+python_version = sys.version_info[0:2]
+python_major_version = python_version[0]
+PY2 = python_major_version == 2
 
 package_dir = os.path.dirname(os.path.realpath(__file__))
 pkg_info = os.path.join(package_dir, "PKG-INFO")
@@ -44,18 +58,11 @@ else:
     frida_extension = os.environ['FRIDA_EXTENSION']
 frida_major_version = int(frida_version.split(".")[0])
 
+index_url_pip_configs = ("global.index-url", "global.extra-index-url")
 
-class UrllibTransport(xmlrpclib.Transport):
-    def __init__(self, *args, **kwargs):
-        xmlrpclib.Transport.__init__(self, *args, **kwargs)
-
-    def request(self, host, handler, request_body, verbose=0):
-        self.verbose = verbose
-        scheme = "https"
-        url = "%(scheme)s://%(host)s%(handler)s" % locals()
-        req = Request(url, data=request_body, headers={'Content-Type': 'text/xml'})
-        fp = urlopen(req)
-        return self.parse_response(fp)
+Tag = namedtuple("Tag", ["tagname", "attrs"])
+ParsedUrlInfo = namedtuple("ParsedUrlInfo",
+                           ["url", "filename", "major", "minor", "micro"])
 
 
 class FridaPrebuiltExt(build_ext):
@@ -69,8 +76,6 @@ class FridaPrebuiltExt(build_ext):
             pass
 
         if in_source_package:
-            python_version = sys.version_info[0:2]
-            python_major_version = python_version[0]
             system = platform.system()
             arch = struct.calcsize('P') * 8
             if system == 'Windows':
@@ -106,36 +111,45 @@ class FridaPrebuiltExt(build_ext):
                 print("prebuilt extension not found in home directory, will try downloading it")
 
                 print("querying pypi for available prebuilds")
-                client = xmlrpclib.ServerProxy("https://pypi.python.org/pypi", transport=UrllibTransport())
-                urls = client.release_urls("frida", frida_version)
+                # index_url is a url compatible with PEP 503
+                index_url = get_index_url().strip()
+                frida_url = urljoin(index_url, "frida/")  # slash is necessary here
+                timeout = 20
+                errmsg = (
+                    "unable to download it within {} seconds; "
+                    "please download it manually to {}"
+                ).format("{}", egg_path)
 
-                urls = [url for url in urls if url['python_version'] != 'source']
+                print("downloading package list from", frida_url)
+                try:
+                    links_html = urlopen(frida_url, timeout=timeout).read().decode("utf-8")
+                except Exception:
+                    print(errmsg.format(timeout))
+                    raise
 
-                if python_major_version >= 3:
-                    urls = [url for url in urls if parse_version(url['python_version'])[0] == python_major_version]
-                else:
-                    urls = [url for url in urls if parse_version(url['python_version']) == python_version]
-
-                os_suffix = "-{}.egg".format(os_version)
-                urls = [url for url in urls if url['filename'].endswith(os_suffix)]
+                parser = PEP503PageParser("frida", frida_version, os_version)
+                parser.feed(links_html)
+                urls = [url for url in parser.urls if url.major == python_major_version]
 
                 if len(urls) == 0:
                     raise NotImplementedError("could not find prebuilt Frida extension; "
                                               "prebuilds only provided for Python 2.7 and 3.4+")
 
                 url = urls[0]
-                egg_url = url['url']
+                egg_url = urljoin(frida_url, url.url)
 
                 try:
                     print("downloading prebuilt extension from", egg_url)
                     timeout = 120  # We'll assume the user has at least 200 kB/s transfer speed.
                     egg_data = urlopen(egg_url, timeout=timeout).read()
-                except Exception as e:
-                    message = "unable to download it within 120 seconds; please download it manually to {}"
-                    print(message.format(egg_path))
-                    raise e
+                except Exception:
+                    print(errmsg.format(timeout))
+                    raise
 
             egg_file = BytesIO(egg_data)
+
+            print("checking hash")
+            check_pep503_hash(egg_file, egg_url)
 
             print("extracting prebuilt extension")
             egg_zip = zipfile.ZipFile(egg_file)
@@ -150,51 +164,148 @@ class FridaPrebuiltExt(build_ext):
             shutil.copyfile(frida_extension, target)
 
 
-def parse_version(version):
-    return tuple(map(int, version.split(".")))
+def get_index_url():
+    """get `index-url` from environment or pip
+    Use FRIDA_INDEX_URL environment variable to customize index-url compatible
+    with PEP 503.
+    """
+    index_url = os.environ.get("FRIDA_INDEX_URL", None)
+    if index_url is not None:
+        return index_url
+
+    for config_name in index_url_pip_configs:
+        try:
+            index_url = get_index_url_from_pip(config_name)
+        except (subprocess.CalledProcessError, OSError):
+            pass
+        else:
+            return index_url
+
+    print("using default index URL: {}".format(DEFAULT_INDEX_URL))
+    return DEFAULT_INDEX_URL
 
 
-setup(
-    name="frida",
-    version=frida_version,
-    description="Dynamic instrumentation toolkit for developers, reverse-engineers, and security researchers",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Frida Developers",
-    author_email="oleavr@frida.re",
-    url="https://frida.re",
-    install_requires=["setuptools"],
-    license="wxWindows Library Licence, Version 3.1",
-    keywords="frida debugger dynamic instrumentation inject javascript windows macos linux ios iphone ipad android qnx",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Environment :: MacOS X",
-        "Environment :: Win32 (MS Windows)",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved",
-        "Natural Language :: English",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: JavaScript",
-        "Topic :: Software Development :: Debuggers",
-        "Topic :: Software Development :: Libraries :: Python Modules"
-    ],
-    packages=['frida'],
-    ext_modules=[Extension('_frida', [])],
-    cmdclass={
-        'build_ext': FridaPrebuiltExt
-    },
-    zip_safe=False
-)
+def get_index_url_from_pip(config_name):
+    assert config_name in index_url_pip_configs
+
+    return subprocess.check_output([sys.executable, "-m", "pip", "config", "get", config_name],
+                                   stderr=subprocess.PIPE)
+
+
+class PEP503PageParser(HTMLParser):
+    def __init__(self, name, version, os_version):
+        HTMLParser.__init__(self)
+        filename_pattern = (
+            r"^{}\-{}\-py(?P<major>\d+)\.(?P<minor>\d+)(\.(?P<micro>\d+))?-{}.egg$"
+        ).format(*map(re.escape, [name, version, os_version]))
+        if PY2:
+            filename_pattern = filename_pattern.decode("utf-8")
+        self._filename_pattern = re.compile(filename_pattern)
+
+    def reset(self):
+        HTMLParser.reset(self)
+        self._path = []
+        self.urls = []
+
+    def handle_starttag(self, tag, attrs):
+        self._path.append(Tag(tag, dict(attrs)))
+
+    def handle_endtag(self, tag):
+        if tag == u"a":
+            while True:
+                if self._path.pop().tagname == tag:
+                    break
+        else:
+            if len(self._path) > 0 and self._path[-1].tagname == tag:
+                self._path.pop()
+
+    def handle_data(self, data):
+        if not (len(self._path) > 0
+                and self._path[-1].tagname == u"a"
+                and self._path[-1].attrs.get("href")):
+            return
+
+        match = self._filename_pattern.match(data)
+        if match is not None:
+            self.urls.append(ParsedUrlInfo(
+                self._path[-1].attrs["href"],
+                data,
+                *map(
+                    lambda g: int(g) if g else None,
+                    map(match.group, ["major", "minor", "micro"])
+                )
+            ))
+
+
+def check_pep503_hash(bytes_io, url):
+    parse_result = urlparse(url)
+    fragment = parse_result.fragment
+    if fragment == "":
+        return
+
+    hashname, hashvalue = fragment.split("=")
+    if hashname not in {"md5", "sha1", "sha224", "sha256", "sha348", "sha512"}:
+        raise ValueError("Unsupported hash algorithm: {}, hashvalue={}".format(
+            hashname, hashvalue,
+        ))
+
+    h = hashlib.new(hashname)
+    for block in iter(partial(bytes_io.read, 4096), b""):  # iterate until EOF
+        h.update(block)
+    digest = h.hexdigest()
+    bytes_io.seek(0)  # reset offset
+
+    if digest == hashvalue:
+        return
+    else:
+        raise ValueError(
+            "`{}` hash checking failed! Expected: {}, but got: {}".format(
+                hashname, hashvalue, digest)
+        )
+
+
+if __name__ == "__main__":
+    setup(
+        name="frida",
+        version=frida_version,
+        description="Dynamic instrumentation toolkit for developers, reverse-engineers, and security researchers",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author="Frida Developers",
+        author_email="oleavr@frida.re",
+        url="https://frida.re",
+        install_requires=["setuptools"],
+        license="wxWindows Library Licence, Version 3.1",
+        keywords="frida debugger dynamic instrumentation inject javascript windows macos linux ios iphone ipad android qnx",
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Environment :: Console",
+            "Environment :: MacOS X",
+            "Environment :: Win32 (MS Windows)",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved",
+            "Natural Language :: English",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: POSIX :: Linux",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: Implementation :: CPython",
+            "Programming Language :: JavaScript",
+            "Topic :: Software Development :: Debuggers",
+            "Topic :: Software Development :: Libraries :: Python Modules"
+        ],
+        packages=['frida'],
+        ext_modules=[Extension('_frida', [])],
+        cmdclass={
+            'build_ext': FridaPrebuiltExt
+        },
+        zip_safe=False
+    )
