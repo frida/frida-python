608ecc4e9bb334364b3036b9ffe3f355b9ee6ff4
Ole André Vadla Ravnås <oleavr@gmail.com>
2022-04-26 09:03:51 +0200

    tests: Use `/bin/cat` as target on most platforms

--- tests/data/__init__.py
+++ tests/data/__init__.py
@@ -2,7 +2,6 @@
 
 import os
 import platform
-import sys
 
 
 system = platform.system()
@@ -11,14 +10,7 @@ if system == 'Windows':
 elif system == 'Darwin':
     target_program = os.path.join(os.path.dirname(__file__), "unixvictim-macos")
 else:
-    machine = platform.machine()
-    if machine == 'aarch64':
-        arch = 'arm64'
-    elif machine.startswith('arm'):
-        arch = 'armhf'
-    else:
-        arch = 'x86_64' if sys.maxsize > 2**32 else 'x86'
-    target_program = os.path.join(os.path.dirname(__file__), "unixvictim-" + system.lower() + "-" + arch)
+    target_program = "/bin/cat"
 
 
 __all__ = ['target_program']
